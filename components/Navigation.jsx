f752c8706ead6add8abdc1ee6ed927dd65da92e1
Joseph <joeseffdl@gmail.com>
2022-11-01 17:38:51 +0800

    Modified landing page

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,19 +4,23 @@ function Navigation() {
 
   return (
     <>
-      <main className="bg-white px-10 min-h-20  dark:bg-gray-800">
-        <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons">Zephy</h1>
-            <ul className="flex items-center">
-                
-                <li>
-                    <a className="
-                        bg-gradient-to-r from-cyan-500 to-teal-500 
-                        text-white px-4 py-2 rounded-md ml-8"
-                        href="#"
-                    >
-                        Resume
-                    </a>
+      <main className="fixed bg-white w-full px-10 min-h-20 z-50">
+        <nav className="py-10 flex align-center justify-center">
+            <ul className="flex items-center gap-10 text-xl font-burtons">
+                <li className="hover:-translate-x-2 ease-in-out duration-300">
+                  <a href="/projects">
+                    Projects
+                  </a>
+                </li> 
+                <li className="hover:-translate-y-2 ease-in-out duration-300">  
+                  <a className="text-2xl font-semibold" href="./">
+                    Zeph
+                  </a>
+                </li>  
+                <li className="hover:translate-x-2 ease-in-out duration-300">
+                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md" href="#">
+                    Resume
+                  </a>
                 </li>
             </ul>
         </nav>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 import Head from "next/head"
 import Image from "next/image"
-import {AiFillTwitterCircle, AiFillFacebook, AiFillYoutube} from "react-icons/ai"
 import zephy from "../public/zephy.jpg"
-import serviceOne from "../public/serviceOne.jpg"
-import serviceTwo from "../public/serviceTwo.jpg"
-import serviceThree from "../public/serviceThree.jpg"
+import react from "../public/react.png"
+import next from "../public/next.png"
+import firebase from "../public/firebase.png"
 import imageOne from "../public/imageOne.jpg"
 import imageTwo from "../public/imageTwo.jpg"
 import imageThree from "../public/imageThree.jpg"
@@ -20,7 +19,8 @@ function Home() {
             <main className="
                 lg:px-40
                 md:px-20
-                px-10">
+                px-10 pt-20
+                ">
                 <section className="py-10">
                     <div className="text-center p-10">
                         <h2 className="
@@ -36,14 +36,10 @@ function Home() {
                         <p className="
                             md:text-xl
                             text-md max-w-lg mx-auto py-5 leading-8 text-gray-800">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis corporis esse expedita iste nulla ut quam nesciunt rem pariatur provident!
+                            Computer Engineering student from Polytechnic University of the Philippines, Mabini Campus. 
                         </p>
                     </div>
-                    <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-                        <AiFillTwitterCircle />
-                        <AiFillFacebook />
-                        <AiFillYoutube />
-                    </div>
+                    
                     <div className="
                         md:h-96 md:w-96
                         relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden">
@@ -52,65 +48,66 @@ function Home() {
                 </section>
                 <section>
                     <div>
-                        <h3 className="text-3xl py-1">Services I offer</h3>
-                        <p className="text-md py-2 leading-8 text-gray-800">Lorem, <span className="text-teal-500">ipsum dolor</span> sit amet consectetur adipisicing elit. Voluptas molestiae quod labore harum ad provident modi eveniet earum accusamus nam voluptate debitis, architecto quae alias deserunt error. Consequuntur, laboriosam.</p>
-                        <p className="text-md py-2 leading-8 text-gray-800">Lorem, <span className="text-teal-500">ipsum dolor</span> sit amet consectetur adipisicing elit. Voluptas molestiae quod labore harum ad provident modi eveniet earum accusamus nam voluptate debitis, architecto quae alias deserunt error. Consequuntur, laboriosam.</p>
+                        <h3 className="text-3xl pt-20 py-2">Main Tech Stack</h3>
+                        <p className="text-md py-2 leading-8 text-gray-800">For building the frontend part of my application, I mainly use <a href="https://reactjs.org/"><span className="text-teal-500">React JS</span></a> which is JavaScript library for building user interfaces. While expanding its capabilities alongside with <a href="https://nextjs.org/"><span className="text-teal-500">Next JS</span></a> to streamline the development process.</p>
+                        <p className="text-md py-2 leading-8 text-gray-800">I also use <a href="https://firebase.google.com/"><span className="text-teal-500">Firebase</span></a> for the backend of my application which I took advantage of its database and authentication services. Wherein I host and deploy my application in <a href="https://vercel.com/"><span className="text-teal-500">Vercel</span></a>.</p>
                     </div>
                     <div className="lg:flex gap-10">
                         <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <Image src={serviceOne} width={100} height={100} alt="Service one image" />
+                            <div className="grid place-content-center">
+                                <Image src={react} width={100} height={100} alt="ReactJS image" />
+                            </div>
                             <h3 className="text-lg font-medium pt-8 pb-2">
-                                Service One
+                                React JS
                             </h3>
                             <p className="py-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto mollitia architecto asperiores maiores, exercitationem quam aliquam quas dicta! Aliquam, iste!
+                                An open-source JavaScript framework and library used for building interactive user interfaces and web applications.    
                             </p>
-                            <h4 className="py-4 text-teal-600">
-                                Tools I used in this service
+                            <h4 className="py-4 text-teal-600 font-semibold">
+                                Implementation
                             </h4>
                             <ul>
-                                <li className="text-gray-800 py-1">Tool 1</li>
-                                <li className="text-gray-800 py-1">Tool 2</li>
-                                <li className="text-gray-800 py-1">Tool 3</li>
-                                <li className="text-gray-800 py-1">Tool 4</li>
+                                <li className="text-gray-800 py-1">Frontend</li>
+                                <li className="text-gray-800 py-1">UI/UX</li>
                             </ul>
                         </div>
                         <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <Image src={serviceTwo} width={100} height={100} alt="Service two image" />
+                            <div className="grid place-content-center">   
+                                <Image src={next} width={100} height={100} alt="NextJS image" />
+                            </div> 
                             <h3 className="text-lg font-medium pt-8 pb-2">
-                                Service Two
+                                Next JS
                             </h3>
                             <p className="py-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto mollitia architecto asperiores maiores, exercitationem quam aliquam quas dicta! Aliquam, iste!
+                                A full-stack framework that can be used to create a website&#39;s user interface on the front end and to manage databases and the rendering process on the back end.
                             </p>
-                            <h4 className="py-4 text-teal-600">
-                                Tools I used in this service
+                            <h4 className="py-4 text-teal-600 font-semibold">
+                                Implementation
                             </h4>
                             <ul>
-                                <li className="text-gray-800 py-1">Tool 1</li>
-                                <li className="text-gray-800 py-1">Tool 2</li>
-                                <li className="text-gray-800 py-1">Tool 3</li>
-                                <li className="text-gray-800 py-1">Tool 4</li>
+                                <li className="text-gray-800 py-1">Backend</li>
+                                <li className="text-gray-800 py-1">Serverless functions</li>
                             </ul>
                         </div>
                         <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <Image src={serviceThree} width={100} height={100} alt="Service three image" />
+                            <div className="grid place-content-center">
+                                <Image src={firebase} width={100} height={100} alt="Firebase image" />
+                            </div>
                             <h3 className="text-lg font-medium pt-8 pb-2">
-                                Service Three
+                                Firebase
                             </h3>
                             <p className="py-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto mollitia architecto asperiores maiores, exercitationem quam aliquam quas dicta! Aliquam, iste!
+                                A service backed by Google which is an application development software that enables developer to develop iOS, Android and Web apps.
                             </p>
-                            <h4 className="py-4 text-teal-600">
-                                Tools I used in this service
+                            <h4 className="py-4 text-teal-600 font-semibold">
+                                Implementation
                             </h4>
                             <ul>
-                                <li className="text-gray-800 py-1">Tool 1</li>
-                                <li className="text-gray-800 py-1">Tool 2</li>
-                                <li className="text-gray-800 py-1">Tool 3</li>
-                                <li className="text-gray-800 py-1">Tool 4</li>
+                                <li className="text-gray-800 py-1">Firestore Database</li>
+                                <li className="text-gray-800 py-1">Authentication</li>
                             </ul>
                         </div>
+                        
                     </div>
                 </section>
                 <section>
@@ -124,44 +121,44 @@ function Home() {
                     <div className="
                         lg:flex-row lg:flex-wrap
                         flex flex-col gap-10 py-10">
-                        <div className="basis-1/3 flex-1">
+                        <div className="basis-1/3 flex-1 shadow-lg p-1 rounded-xl">
                             <Image
-                                className="rounded-lg object-cover"
+                                className="rounded-lg object-cover mx-auto"
                                 width={"100%"}
                                 height={"100%"}
                                 layout="responsive"
                                 src={imageOne} alt="Image" />
                         </div>
-                        <div className="basis-1/3 flex-1">
+                        <div className="basis-1/3 flex-1 shadow-lg p-10 rounded-xl">
                             <Image
-                                className="rounded-lg object-cover"
+                                className="rounded-lg object-cover mx-auto"
                                 width={"100%"}
                                 height={"100%"}
                                 layout="responsive"
                                 src={imageTwo}
                                 alt="Image" />
                         </div>
-                        <div className="basis-1/3 flex-1">
+                        <div className="basis-1/3 flex-1 shadow-lg p-1 rounded-xl">
                             <Image
-                                className="rounded-lg object-cover"
+                                className="rounded-lg object-cover mx-auto"
                                 width={"100%"}
                                 height={"100%"}
                                 layout="responsive"
                                 src={imageThree}
                                 alt="Image" />
                         </div>
-                        <div className="basis-1/3 flex-1">
+                        <div className="basis-1/3 flex-1 shadow-lg p-2 rounded-xl">
                             <Image
-                                className="rounded-lg object-cover"
+                                className="rounded-lg object-cover mx-auto"
                                 width={"100%"}
                                 height={"100%"}
                                 layout="responsive"
                                 src={imageFour}
                                 alt="Image" />
                         </div>
-                        <div className="basis-1/3 flex-1">
+                        <div className="basis-1/3 flex-1 shadow-lg p-6 rounded-xl">
                             <Image
-                                className="rounded-lg object-cover"
+                                className="rounded-lg object-cover mx-auto"
                                 width={"100%"}
                                 height={"100%"}
                                 layout="responsive"
