071beca6b95fbb07b8cf68ccc730e0d2b0573377
Joseph De Leon <joeseffdl@gmail.com>
2023-03-23 20:11:10 +0800

    Removed unused materials and converted jsx files to tsx

--- components/InsightCard.tsx
+++ components/InsightCard.tsx
@@ -1,7 +1,14 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
 
-const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+type InsightProps = {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+  index: number;
+};
+
+const InsightCard = ({ imgUrl, title, subtitle, index }: InsightProps) => (
   <motion.div
     variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
